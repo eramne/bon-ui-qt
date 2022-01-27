43981a1378ce6887d094ee664c92b49e990ebc73
eramne <79015185+eramne@users.noreply.github.com>
2022-01-27 13:47:47 -0500

    HAHA YES i was able to hide that useless warning that kept annoying me

--- demo/main.cpp
+++ demo/main.cpp
@@ -2,6 +2,14 @@
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
 
+void customMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    if (!msg.contains("QML Shortcut: Shortcut: Only binding to one of multiple key bindings associated with")) {
+        fprintf(stdout, qFormatLogMessage(type, context, msg).toLocal8Bit()+"\n");
+        fflush(stdout);
+    }
+}
+
 int main(int argc, char* argv[]) {
     #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
         QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -12,12 +20,7 @@ int main(int argc, char* argv[]) {
     QQmlApplicationEngine engine;
     engine.addImportPath(BON_PATH);
     qputenv("QML_XHR_ALLOW_FILE_READ", QByteArray("1"));
-
-    QLoggingCategory::setFilterRules(
-        //"*.warning=false\n"
-        "js=true\n"
-        "qml=true\n"
-    );
+    qInstallMessageHandler(customMessageOutput);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
